572f98ae0a1b719cbcb543a2a45665e409d2b08d
enthusiast17 <ulan17nurym@gmail.com>
2021-03-28 21:39:14 +0500

    fix: remove row in card list

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Col, Grid, Row } from 'rsuite';
+import { Col, Grid } from 'rsuite';
 import Card from './Card/Card';
 
 const CardList = ({ cards }) => {
   return (
     <Grid>
       {cards.map((row, rowKey) => (
-        <Row key={rowKey} className="show-grid">
-          {row.map((col, colKey) => (
-            <Col key={colKey} xs={24} md={12} lg={6} style={{ padding: 10 }}>
-              <Card
-                content={col.content}
-                color={col.color}
-                action={col.action}
-              />
-            </Col>
-          ))}
-        </Row>
+        row.map((col, colKey) => (
+          <Col key={colKey} xs={24} md={12} lg={row.length === 4 ? 6 : 8} style={{ padding: 10 }}>
+            <Card
+              content={col.content}
+              color={col.color}
+              action={col.action}
+            />
+          </Col>
+        ))
       ))}
     </Grid>
   )
