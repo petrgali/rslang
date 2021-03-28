a4766fc26b0d9c6ab04acdaa0e56acdf2dae7587
petrgali <opelliek@gmail.com>
2021-03-28 17:23:16 +0600

    refactor: replace function to object

--- src/services/constant.js
+++ src/services/constant.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = "https://enthusiast17-rslang-api.herokuapp.com/"
 export const USER = {
-    TOKEN: "acces_token",
+    TOKEN: "access_token",
     ID: "id",
 }
 export const ERROR = {
--- src/services/interfaceAPI.js
+++ src/services/interfaceAPI.js
@@ -5,198 +5,193 @@ const setStorage = (...props) => {
     props.map(obj => localStorage.setItem(obj.name, obj.payload))
 }
 
-const interactAPI = () => {
-    const postNoAuth = {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+const postNoAuth = {
+    method: "POST",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+}
+const postAuth = {
+    method: "POST",
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem(USER.TOKEN)}`,
+        ...postNoAuth.headers,
+    },
+}
+const putAuth = {
+    method: "PUT",
+    headers: { ...postAuth.headers }
+}
+const deleteAuth = {
+    method: "DELETE",
+    headers: { ...postAuth.headers }
+}
+const getAuth = {
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem(USER.TOKEN)}`,
+        'Accept': 'application/json',
+    }
+}
+
+const requestAPI = async (options) => {
+    try {
+        let response = await axios(options)
+        return {
+            status: response.status,
+            payload: response.data
+        }
+    } catch ({ message }) {
+        let status = Number.parseInt(message.split(" ").pop())
+        return {
+            status: status,
+            payload: ERROR[status]
         }
     }
-    const postAuth = {
-        method: "POST",
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem(USER.TOKEN)}`,
-            ...postNoAuth.headers,
-        },
-    }
-    const putAuth = {
-        method: "PUT",
-        headers: { ...postAuth.headers }
-    }
-    const deleteAuth = {
-        method: "DELETE",
-        headers: { ...postAuth.headers }
-    }
-    const getAuth = {
-        headers: {
-            'Authorization': `Bearer ${localStorage.getItem(USER.TOKEN)}`,
-            'Accept': 'application/json',
+}
+const interactAPI = {
+    // endpoints with no required registration
+    getWords: (page = 0, group = 0) => {
+        return requestAPI({
+            url: `${API_BASE_URL}words?page=${page}&group=${group}`,
+        })
+    },
+    getWordbyId: (id) => {
+        return requestAPI({
+            url: `${API_BASE_URL}words/${id}`,
+        })
+    },
+    /* newUser schema sample 
+    {
+    "name": "string",
+    "email": "string",
+    "password": "string"
+    }*/
+    registerUser: (user) => {
+        return requestAPI({
+            ...postNoAuth,
+            url: `${API_BASE_URL}users`,
+            data: JSON.stringify(user)
+        })
+    },
+    /* signin schema sample
+    {
+    "email": "string",
+    "password": "string"
+    }*/
+    loginUser: async (user) => {
+        let response = await requestAPI({
+            ...postNoAuth,
+            url: `${API_BASE_URL}signin`,
+            data: JSON.stringify(user),
+        })
+        if (response.status === 200) {
+            setStorage({
+                name: USER.TOKEN,
+                payload: response.payload.token
+            }, {
+                name: USER.ID,
+                payload: response.payload.userId
+            })
+            return { status: response.status }
         }
-    }
-    const requestAPI = async (options) => {
-        try {
-            let response = await axios(options)
-            return {
-                status: response.status,
-                payload: response.data
-            }
-        } catch ({ message }) {
-            let status = Number.parseInt(message.split(" ").pop())
-            return {
-                status: status,
-                payload: ERROR[status]
-            }
-        }
-    }
-
-    return {
-        // endpoints with no required registration
-        getWords(page = 0, group = 0) {
-            return requestAPI({
-                url: `${API_BASE_URL}words?page=${page}&group=${group}`,
-            })
-        },
-        getWordbyId(id) {
-            return requestAPI({
-                url: `${API_BASE_URL}words/${id}`,
-            })
-        },
-
-        /* newUser schema sample 
-        {
-        "name": "string",
-        "email": "string",
-        "password": "string"
-        }*/
-        newUser(user) {
-            return requestAPI({
-                ...postNoAuth,
-                url: `${API_BASE_URL}users`,
-                data: JSON.stringify(user)
-            })
-        },
-
-        /* signin schema sample
-        {
-        "email": "string",
-        "password": "string"
-        }*/
-        async signin(user) {
-            let response = await requestAPI({
-                ...postNoAuth,
-                url: `${API_BASE_URL}signin`,
-                data: JSON.stringify(user),
-            })
-            if (response.status === 200) {
-                setStorage({
-                    name: USER.TOKEN,
-                    payload: response.payload.token
-                }, {
-                    name: USER.ID,
-                    payload: response.payload.userId
-                })
-                return { status: response.status }
-            }
-            return { status: response.status, payload: response.payload }
-        },
-        // USERS JWT required 
-        getUserbyId(id = localStorage.getItem(USER.ID)) {
-            return requestAPI({
-                url: `${API_BASE_URL}users/${id}`,
-                ...getAuth,
-            })
-        },
-        // USERS/WORDS JWT required
-        getUserWords(id = localStorage.getItem(USER.ID)) {
-            return requestAPI({
-                url: `${API_BASE_URL}users/${id}/words`,
-                ...getAuth,
-            })
-        },
-
-        /*word setting schema sample 
-        {
-        "difficulty": "string", (hard, deleted)
-        "optional": {}          (isLearning: bool)
-        }*/
-        addUserWord(wordId, setting) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...postAuth,
-                url: `${API_BASE_URL}users/${id}/words/${wordId}`,
-                data: JSON.stringify(setting)
-            })
-        },
-        getUserWordbyId(wordId) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...getAuth,
-                url: `${API_BASE_URL}users/${id}/words/${wordId}`,
-            })
-        },
-        /*word setting schema sample 
-        {
-        "difficulty": "string", (hard, deleted)
-        "optional": {}          (isLearning: bool)
-        }*/
-        updateUserWordbyId(wordId, setting) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...putAuth,
-                url: `${API_BASE_URL}users/${id}/words/${wordId}`,
-                data: JSON.stringify(setting)
-            })
-        },
-        deleteUserWordbyId(wordId) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...deleteAuth,
-                url: `${API_BASE_URL}users/${id}/words/${wordId}`,
-            })
-        },
-        // USERS/Statistic JWT required
-        getUserStat() {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...getAuth,
-                url: `${API_BASE_URL}users/${id}/statistics`,
-            })
-        },
-        /* user stat schema sample
-        {
-        "learnedWords": 0,
-        "optional": {}
-        }*/
-        updateUserStat(stat) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...putAuth,
-                url: `${API_BASE_URL}users/${id}/statistics`,
-                data: JSON.stringify(stat),
-            })
-        },
-        //USER/Setting JWT required
-        getUserSettings() {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...getAuth,
-                url: `${API_BASE_URL}users/${id}/settings`,
-            })
-        },
-        /* user settings schema sample
-        {
-        "wordsPerDay": 0,
-        "optional": {}
-        }*/
-        updateUserSettings(setting) {
-            let id = localStorage.getItem(USER.ID)
-            return requestAPI({
-                ...putAuth,
-                url:`${API_BASE_URL}users/${id}/settings`,
-                data: JSON.stringify(setting),
-            })
-        }
+        return { status: response.status, payload: response.payload }
+    },
+    // USERS JWT required 
+    getUserbyId: (id = localStorage.getItem(USER.ID)) => {
+        return requestAPI({
+            ...getAuth,
+            url: `${API_BASE_URL}users/${id}`,
+        })
+    },
+    // USERS/WORDS JWT required
+    getUserWords: (id = localStorage.getItem(USER.ID)) => {
+        return requestAPI({
+            ...getAuth,
+            url: `${API_BASE_URL}users/${id}/words`,
+        })
+    },
+    /*word setting schema sample 
+    {
+    "difficulty": "string", (hard, deleted)
+    "optional": {}          (isLearning: bool)
+    }*/
+    addUserWord: (wordId, setting) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...postAuth,
+            url: `${API_BASE_URL}users/${id}/words/${wordId}`,
+            data: JSON.stringify(setting)
+        })
+    },
+    getUserWordbyId: (wordId) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...getAuth,
+            url: `${API_BASE_URL}users/${id}/words/${wordId}`,
+        })
+    },
+    /*word setting schema sample 
+    {
+    "difficulty": "string", (hard, deleted)
+    "optional": {}          (isLearning: bool)
+    }*/
+    updateUserWordbyId: (wordId, setting) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...putAuth,
+            url: `${API_BASE_URL}users/${id}/words/${wordId}`,
+            data: JSON.stringify(setting)
+        })
+    },
+    deleteUserWordbyId: (wordId) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...deleteAuth,
+            url: `${API_BASE_URL}users/${id}/words/${wordId}`,
+        })
+    },
+    // USERS/Statistic JWT required
+    getUserStat: () => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...getAuth,
+            url: `${API_BASE_URL}users/${id}/statistics`,
+        })
+    },
+    /* user stat schema sample
+    {
+    "learnedWords": 0,
+    "optional": {}
+    }*/
+    updateUserStat: (stat) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...putAuth,
+            url: `${API_BASE_URL}users/${id}/statistics`,
+            data: JSON.stringify(stat),
+        })
+    },
+    //USER/Setting JWT required
+    getUserSettings: () => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...getAuth,
+            url: `${API_BASE_URL}users/${id}/settings`,
+        })
+    },
+    /* user settings schema sample
+    {
+    "wordsPerDay": 0,
+    "optional": {}
+    }*/
+    updateUserSettings: (setting) => {
+        let id = localStorage.getItem(USER.ID)
+        return requestAPI({
+            ...putAuth,
+            url: `${API_BASE_URL}users/${id}/settings`,
+            data: JSON.stringify(setting),
+        })
     }
 }
 export default interactAPI
