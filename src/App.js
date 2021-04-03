8b78a8bc45d539b48c8f9d3c0d142a7e3570f747
enthusiast17 <ulan17nurym@gmail.com>
2021-04-03 19:10:41 +0500

    feat: add menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import { BrowserRouter } from "react-router-dom"
+import { Container, Content, Footer, Header, Sidebar } from "rsuite";
 import { RouterConfig } from "./navigation/RouterConfig"
 import setDefaults from "./services/setOptionsDefault"
-import 'rsuite/dist/styles/rsuite-default.css';
+import "rsuite/dist/styles/rsuite-default.css";
+import Menu from "./components/Menu/Menu";
 import "./styles/App.css";
 
 function App() {
   setDefaults()
   return (
     <BrowserRouter>
-      <RouterConfig />
+      <Container className="container">
+        <Sidebar className="sidebar" style={{ flex: 0, width: 56 }}>
+          <Menu />
+        </Sidebar>
+        <Container>
+          <Header>
+
+          </Header>
+          <Content>
+              <RouterConfig />
+          </Content>
+          <Footer>
+            
+          </Footer>
+        </Container>
+      </Container>
     </BrowserRouter>
   )
 }
