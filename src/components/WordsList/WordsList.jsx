434bfca57cfffe38c8d765d97b9384d1a0b56783
petrgali <opelliek@gmail.com>
2021-04-06 13:04:58 +0600

    fix: fixed word status handling on data updates

--- src/components/WordsList/WordsList.jsx
+++ src/components/WordsList/WordsList.jsx
@@ -8,19 +8,19 @@ function WordsList({ data, setWordStatus, recoverWord, showControl, showTranslat
   return (
     <List className="words-list">
       {data.map((obj) =>
-        (<List.Item key={obj._id}>
-          <WordBox
-            showTranslate={showTranslate}
-            word={obj}
-            buttons={<WordButtons
-              showControl={showControl}
-              isWordHard={obj.userWord && obj.userWord.difficulty === "hard"}
-              showRecover={showRecover}
-              recoverWord={() => recoverWord(obj._id)}
-              setWordHard={() => setWordStatus(obj._id, STATUS.HARD)}
-              removeWord={() => setWordStatus(obj._id, STATUS.DELETED)} />}
-            />
-        </List.Item>)
+      (<List.Item key={obj._id}>
+        <WordBox
+          showTranslate={showTranslate}
+          word={obj}
+          buttons={<WordButtons
+            showControl={showControl}
+            isWordHard={obj.userWord && obj.userWord.difficulty === "hard"}
+            showRecover={showRecover}
+            recoverWord={() => recoverWord(obj._id)}
+            setWordHard={() => setWordStatus(obj._id, obj.userWord, STATUS.HARD)}
+            removeWord={() => setWordStatus(obj._id, obj.userWord, STATUS.DELETED)} />}
+        />
+      </List.Item>)
       )}
     </List>
   )
